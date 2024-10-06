6f7ddb7e246c32d019d754410e9cfd882aadf89d
ultra-rony <i@rodrix.ru>
2024-10-06 09:35:39 +0900

    Add localization

--- lib/app/app.dart
+++ lib/app/app.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_localizations/flutter_localizations.dart';
 
+import '../generated/l10n.dart';
 import 'app_themes.dart';
 import 'routes.dart';
 
@@ -9,6 +11,13 @@ class App extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp.router(
+        localizationsDelegates: const [
+          S.delegate,
+          GlobalMaterialLocalizations.delegate,
+          GlobalWidgetsLocalizations.delegate,
+          GlobalCupertinoLocalizations.delegate,
+        ],
+        supportedLocales: S.delegate.supportedLocales,
         routerConfig: router,
         theme: lightTheme,
         debugShowCheckedModeBanner: false);
