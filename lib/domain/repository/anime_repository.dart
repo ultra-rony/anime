d49887ba42edc28261d734314dcbb36215431101
ultra-rony <i@rodrix.ru>
2024-10-06 21:47:11 +0900

    Add anime_repository.dart

---
+++ lib/domain/repository/anime_repository.dart
@@ -0,0 +1,6 @@
+import '../../core/resources/date_state.dart';
+import '../entities/anime_entity.dart';
+
+abstract class AnimeRepository {
+  Future<DataState<List<AnimeEntity>>> getRemoteAnime(int page, int limit);
+}
