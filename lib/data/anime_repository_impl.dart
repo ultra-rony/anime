6ff66e367b6f7661764cdc93fd25da4dc45d9847
ultra-rony <i@rodrix.ru>
2024-10-06 22:00:45 +0900

    Add anime_repository_impl.dart

---
+++ lib/data/anime_repository_impl.dart
@@ -0,0 +1,11 @@
+import 'package:anime/core/resources/date_state.dart';
+import 'package:anime/domain/entities/anime_entity.dart';
+import 'package:anime/domain/repository/anime_repository.dart';
+
+class AnimeRepositoryImpl implements AnimeRepository {
+
+  @override
+  Future<DataState<List<AnimeEntity>>> getRemoteAnime(int page, int limit) {
+    throw UnimplementedError();
+  }
+}
